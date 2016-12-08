76d009a9c6d7e5a4b657d65ee6728195c6f7cdcb
David Loring <David Loring>
2016-12-08 16:56:39 -0500

    add dummy day class

--- backend.py
+++ backend.py
@@ -1,5 +1,8 @@
 class excercise:
+    """A class containing a dict of excercise attributes and a timestamp"""
     def __init__(values, time):
         self.details = dict(values)
         self.timestamp = time
-    
+class day():
+    """contains excercises"""
+    pass
