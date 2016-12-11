5fe3b98e337644b94f9e5e38a7af64b18078eace
David Loring <david@dthl.me>
2016-12-11 14:44:14 -0500

    add default args and init function

--- backend.py
+++ backend.py
@@ -1,11 +1,17 @@
 class excercise:
     """A class containing a dict of excercise attributes and a timestamp"""
-    def __init__(self, values, time):
+    def __init__(self, time, values=None):
         self.details = dict(values)
         self.timestamp = time
 class day():
     """contains excercises and a date"""
+    def __init__(self, date, excer_list=None):
+        self.excercises = excer_list
+        self.datestamp = date
     pass
 class week():
-    """contains days"""
-    pass
+    """contains 7 consecutive days"""
+    def __init__(self, start_date):
+        self.daylist = list()
+        for i in range(7):
+            self.daylist.append(day(date=(start_date+i)))
