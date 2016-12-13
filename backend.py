3a792a70dfeb48ff3384b6095ab916853450eeb6
tangibleghost <dloring@Davids-MacBook-Air.local>
2016-12-13 17:41:19 -0500

    add weight function

--- backend.py
+++ backend.py
@@ -1,8 +1,12 @@
 class excercise:
     """A class containing a dict of excercise attributes and a timestamp"""
     def __init__(self, time, values=None):
-        self.details = dict(values)
+        self.details = values
         self.timestamp = time
+    def add_weights(self, weight, reps):
+        self.details['type'] = 'weights'
+        self.details['weight'] = weight
+        self.details['reps'] = reps
 class day():
     """contains excercises and a date"""
     def __init__(self, date, excer_list=None):
